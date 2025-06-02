2d6e50de2c84a72e8aa1f5e0d4420b5cce8c1bf8
Hank <lovebaonvwu@gmail.com>
2025-06-02 21:44:21 +0800

    implement tokenizerv2

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import re
+
+from simple_tokenizer import SimpleTokenizerV1, SimpleTokenizerV2
+
 text = "Hello, world. This, is a test."
 result = re.split(r'([,.]|\s)', text)
 print(result)
@@ -33,3 +36,33 @@ for i, item in enumerate(vocab.items()):
     if i > 50:
         break
 
+tokenizer = SimpleTokenizerV1(vocab)
+
+text = """"It's the last he painted, you know," Mrs. Gisburn said with pardonable
+pride."""
+ids = tokenizer.encode(text)
+print(ids)
+print(tokenizer.decode(ids))
+
+text = "Hello, do you like tea?"
+# pop KeyError: 'Hello' not in vocab
+# print(tokenizer.encode(text))
+
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+
+vocab = {token:integer for integer,token in enumerate(all_tokens)}
+
+print(len(vocab.items()))
+
+for i, item in enumerate(list(vocab.items())[-5:]):
+    print(item)
+
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+print(text)
+
+tokenizer = SimpleTokenizerV2(vocab)
+print(tokenizer.encode(text))
+print(tokenizer.decode(tokenizer.encode(text)))
